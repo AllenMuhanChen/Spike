a4fdea15778825b26fe2f456f7ebadf463a2ccb9
Allen Chen <allenmuhanchen@gmail.com>
2023-09-20 09:09:31 -0400

    Change preprocess path for mac

--- preprocess.m
+++ preprocess.m
@@ -1,6 +1,6 @@
 
 fclose('all'); clear; clc
-expFolder = '/home/r2_allen/Documents'; %base path to experiment folder
+expFolder = '/Users/allenchen/Documents'; %base path to experiment folder
 animalID = 'mdemo'; unitID = '000'; expID = '000'; probeID = 1;
 name = 'AC'; copyToZ = 0; MUflag = 0; legacyFlag = 0;  parts = 100;
 
